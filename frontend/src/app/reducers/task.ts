19f96a92e4e6887a526533ac632ca57e97b9799d
DESKTOP-D314UU8\khangle2307 <khang.le@blueoc.tech>
2024-05-16 11:24:15 +0700

    setup base project

---
+++ frontend/src/app/reducers/task.ts
@@ -0,0 +1,11 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const taskSLice = createSlice({
+  name: "tasks",
+  initialState,
+  reducers: {},
+});
+
+export default taskSLice.reducer;
