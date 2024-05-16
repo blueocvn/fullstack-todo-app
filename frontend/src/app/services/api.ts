49b9cabf48a98b990418b5d4405d028a789c3de4
blueoc-dathoang <dat.hoang@blueoc.tech>
2024-05-16 15:18:45 +0700

    Revert "ERP-580 : Create UI for register user"

--- frontend/src/app/services/api.ts
+++
@@ -1,9 +0,0 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-
-export const api = createApi({
-  reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BACKEND_API,
-  }),
-  endpoints: () => ({}),
-});
