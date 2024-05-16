ba6775e10ffce83eca4cc019272d3e8ea8e12c25
Hoangka12345 <hoanghaiphong280320@gmail.com>
2024-05-16 09:34:42 +0700

    feat:build code base for back-end

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends , Request , Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBearer
 
-app = FastAPI()
+from app.core.config import settings
+from app.core.database import SessionLocal
+from app.apis.todo import todo_router
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def create_application() -> FastAPI:
+    application = FastAPI(title=settings.PROJECT_NAME)
+    return application
+
+app = create_application()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=[settings.CORS_ORIGINS],
+    allow_methods=[settings.CORS_METHODS],
+    allow_headers=[settings.CORS_HEADERS],
+)
+
+
+app.include_router(todo_router)
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
---
+++ backend/app/services/task.py
@@ -0,0 +1,5 @@
+class TaskService:
+    def get_all():
+        return {
+            "data": ["hoang","son","ly"]
+        }
