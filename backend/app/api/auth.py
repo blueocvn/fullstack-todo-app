0d147cb3c61cd3747916f2fb989cfc142af530c3
blueoc-hoangphan <hoang.phan@blueoc.tech>
2024-05-18 21:29:43 +0700

    feat: build login api

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.schemas.auth import Register
+from app.schemas.auth import Register, Login
 from app.core.database import get_db
 from app.services.auth import AuthService
 
@@ -10,3 +10,7 @@ router = APIRouter()
 @router.post("/register")
 def register(payload:Register, db:Session = Depends(get_db)): 
     return AuthService.register(db, payload)
+
+@router.post("/login")
+def login(payload:Login, db:Session = Depends(get_db)): 
+    return AuthService.login(db, payload)
--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -4,3 +4,7 @@ class Register(BaseModel):
     username: str
     email: EmailStr
     password:str
+
+class Login(BaseModel):
+    email:EmailStr
+    password:str
