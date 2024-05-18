12fb36405e35a2e3871d6dcc8d8f63926c9801d0
blueoc-hoangphan <hoang.phan@blueoc.tech>
2024-05-18 11:52:58 +0700

    feat: build register api

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,3 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.schemas.auth import Register
+from app.core.database import get_db
+from app.services.auth import AuthService
 
 router = APIRouter()
+
+@router.post("/register")
+def register(payload:Register, db:Session = Depends(get_db)): 
+    return AuthService.register(db, payload)
