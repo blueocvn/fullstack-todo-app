3ae4c452144c86bf423dfe645067047b1acf5eb4
blueoc-QuanND <quan.nguyendang@blueoc.tech>
2024-05-20 15:17:23 +0700

    conflix

--- frontend/src/components/form/ChangePasswordFrom.tsx
+++ frontend/src/components/form/ChangePasswordFrom.tsx
@@ -1,24 +1,25 @@
-import { Container, Row } from 'react-bootstrap'
-import { FloatingLabel, Button, Card } from "flowbite-react";
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 const ChangePasswordForm = () => {
     return (
-        <Container>
-            <Card className="max-w-m">
-                <Row>
-                    <h1>Nhập Mật Khẩu Mới</h1>
-                    <FloatingLabel variant="outlined" label="Password" />
-                </Row>
-                <Row>
-                    <h1>Nhập Lại Mật Khẩu</h1>
-                    <FloatingLabel variant="outlined" label="Re-Password" />
-                </Row>
-                <Row>
-                    <Button color="blue" pill>
-                        Đổi Mật Khẩu
-                    </Button>
-                </Row>
-            </Card>
-        </Container>
+        <form className="flex max-w-md flex-col gap-4">
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="email1" value="Your email" />
+        </div>
+        <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="password1" value="Your password" />
+        </div>
+        <TextInput id="password1" type="password" required />
+      </div>
+      <div className="flex items-center gap-2">
+        <Checkbox id="remember" />
+        <Label htmlFor="remember">Remember me</Label>
+      </div>
+      <Button type="submit">Submit</Button>
+    </form>
     )
 }
 
